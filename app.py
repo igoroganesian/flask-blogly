04b4a6728a23399b3fba0021c97e760091495373
Igor Oganesian <igor.oganesian@gmail.com>
2023-07-26 15:32:40 -0400

    added templates, working on detail page

--- app.py
+++ app.py
@@ -2,13 +2,18 @@
 
 import os
 
-from flask import Flask
-from models import connect_db
+from flask import Flask, request, render_template, redirect
+from models import connect_db, db, User
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     "DATABASE_URL", 'postgresql:///blogly')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'secret'
+
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
